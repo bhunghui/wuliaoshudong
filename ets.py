97d7973372e129ddaa6d290150224b9112a69907
iobhunghui <iobhunghui@gmail.com>
2023-04-20 22:03:01 +0800

    commit

--- ets.py
+++ ets.py
@@ -1,3 +1,3 @@
 from werkzeug.security import check_password_hash
 
-print(check_password_hash('$1f6ac0890dd4b8a656ce45b45e99525e5026f7a1309d10884928dbffc69e3972','ywhywhywh',salt_length=9))
+print(check_password_hash('$1f6ac0890dd4b8a656ce45b45e99525e5026f7a1309d10884928dbffc69e3972','ywhywhywh'))
--- form.py
+++ form.py
@@ -1,18 +1,23 @@
 import wtforms
-from wtforms.validators import length,email,EqualTo
+from wtforms.validators import length, email, EqualTo
+
+
 class LoginForm(wtforms.Form):
-    username = wtforms.StringField(validators=[length(min=1,max=10)])
-    passwd = wtforms.StringField(validators=[length(min=8,max=30)])
+    username = wtforms.StringField(validators=[length(min=1, max=10)])
+    passwd = wtforms.StringField(validators=[length(min=8, max=30)])
+
 
 class RegisterForm(wtforms.Form):
-    username = wtforms.StringField(validators=[length(min=1,max=10)])
-    passwd = wtforms.StringField(validators=[length(min=8,max=30)])
+    username = wtforms.StringField(validators=[length(min=1, max=10)])
+    passwd = wtforms.StringField(validators=[length(min=8, max=30)])
     passwd_again = wtforms.StringField(validators=[EqualTo('passwd')])
     email = wtforms.StringField(validators=[email()])
 
+
 class QaForm(wtforms.Form):
-    title = wtforms.StringField(validators=[length(min= 10,max=150)])
-    content = wtforms.StringField(validators=[length(min=20,max=500)])
+    title = wtforms.StringField(validators=[length(min=10, max=150)])
+    content = wtforms.StringField(validators=[length(min=20, max=500)])
+
 
 class CommentForm(wtforms.Form):
     content = wtforms.StringField(validators=[length(min=1, max=800)])
